0577d211b5b893de433a034c8a1dee2ff30d8fab
Mateus Valentim <38231569+MateusValentim@users.noreply.github.com>
2021-09-09 22:42:56 -0300

    Functions

--- src/Funcoes.java
+++ src/Funcoes.java
@@ -2,7 +2,7 @@ import java.util.function.Function;
 
 public class Funcoes {
     public static void main(String[] args) {
-
+//COMMENT
         Function<String, String> retornaNomeAoContrario = texto -> new StringBuilder(texto).reverse().toString();
 
         Function<String, Integer> converteStringParaInteiroECalculaODobro = string -> Integer.valueOf(string) * 2;
